f19e08af057bbbb64da4ff45499e5cebc3a4d0ce
Anton <leshyovantoha@yandex.ru>
2025-07-20 13:51:05 +0500

    add logger

---
+++ internal/transport/grpc/interceptor.go
@@ -0,0 +1,15 @@
+package grpc
+
+import (
+	"context"
+	"log/slog"
+	"github.com/tainj/distributed_calculator2/pkg/logger"
+	"google.golang.org/grpc"
+)
+
+func ContextWithLogger(l logger.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+		l.Info(ctx, "request started", slog.String("method", info.FullMethod))
+		return handler(ctx, req)
+	}
+}
